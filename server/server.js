bc7d88c1c36cddb260999c546b697de7f1f54103
T-Letuka <tisetsoletuka@gmail.com>
2024-06-19 17:43:50 +0200

    fixed server side for deployment

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { pool } = require("./db");
 const dotenv = require("dotenv");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const path = require("path");
 
